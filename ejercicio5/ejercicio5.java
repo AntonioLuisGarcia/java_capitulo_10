9cb0d913c94f508d563e38c32ed41436ecee05d1
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-23 23:29:21 +0100

    clase disco

---
+++ ejercicio5/ejercicio5.java
@@ -0,0 +1,22 @@
+package ejercicio5;
+/*
+* Ejercicio 5
+*
+* Realiza de nuevo el ejercicio de la colección de discos pero utilizando esta vez una lista para almacenar la información 
+* sobre los discos en lugar de un array convencional. Comprobarás que el código se simplifica notablemente ¿Cuánto ocupa el 
+* programa original hecho con un array? ¿Cuánto ocupa este nuevo programa hecho con una lista?
+* 
+* @author Antonio Luis Garcia
+*/
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ejercicio5{
+    public static void main(String[] args) {
+
+        ArrayList <Disco> coleccion = new ArrayList <Disco>();
+
+
+    }
+}
