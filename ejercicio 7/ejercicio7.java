cc7380c319b215691dc32896c98b3690b5eb3a9c
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-24 22:51:44 +0100

    main

--- ejercicio 7/ejercicio7.java
+++ ejercicio 7/ejercicio7.java
@@ -17,9 +17,22 @@
  *  @author Antonio Luis Garcia
  */
 
+import java.util.ArrayList;;
 
 public class ejercicio7 {
+
     public static void main(String[] args) {
+
+        ArrayList <Eurocoin> a = new ArrayList<>();
+
+        for(int i = 0 ; i < 6 ; i++){
+            Eurocoin euro = new Eurocoin();
+            a.add(euro);
+        }
+
+        for(Eurocoin e : a){
+            System.out.println(e);
+        }
         
     }
     
