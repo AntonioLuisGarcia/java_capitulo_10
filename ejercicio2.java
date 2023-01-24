f2c8e2eae6775578f256c396cd1bcffef8e7fd26
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-24 22:26:26 +0100

    enunciado

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,5 +1,5 @@
 /*
-*Ejercicio 2
+* Ejercicio 2
 *
 * Realiza un programa que introduzca valores aleatorios (entre 0 y 100) en un ArrayList y que luego calcule la suma, 
 * la media, el máximo y el mínimo de esos números. El tamaño de la lista también será aleatorio y podrá oscilar entre 10
