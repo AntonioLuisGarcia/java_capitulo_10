74e2da0a42f90d7fbef8926b1ba56132624225a9
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-26 21:52:08 +0100

    enunciado

--- ejercicio14/ejercicio14.java
+++ ejercicio14/ejercicio14.java
@@ -1,5 +1,20 @@
 package ejercicio14;
 
+/*
+ *
+ * Ejercicio 14.
+ * 
+ *  Un supermercado de productos ecológicos nos ha pedido hacer un programa para vender su mercancía. En esta 
+ * primera versión del programa se tendrán en cuenta los productos que se indican en la tabla junto con su precio.
+ * Los productos se venden en bote, brick, etc. Cuando se realiza la compra, hay que indicar el producto y el número 
+ * de unidades que se compran, por ejemplo “guisantes” si se quiere comprar un bote de guisantes y la cantidad, por
+ * ejemplo “3” si se quieren comprar 3 botes. La compra se termina con la palabra “fin. Suponemos que el usuario no 
+ * va a intentar comprar un producto que no existe. Utiliza un diccionario para almacenar los nombres y precios de los
+ * productos y una o varias listas para almacenar la compra que realiza el usuario.
+ * 
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
