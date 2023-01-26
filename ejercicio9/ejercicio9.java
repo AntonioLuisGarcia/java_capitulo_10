37f2bfc3cd8c203b91cef7d0c382ec4db9276245
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-26 08:27:13 +0100

    enunciado de ejercicio

--- ejercicio9/ejercicio9.java
+++ ejercicio9/ejercicio9.java
@@ -1,7 +1,15 @@
+/*
+* Ejercicio 9.
+* 
+* Modifica el programa anterior de tal forma que las cartas se muestren ordenadas. Primero se ordenarán por palo: bastos, 
+* copas, espadas, oros. Cuando coincida el palo, se ordenará por número: as, 2, 3, 4, 5, 6, 7, sota, caballo, rey.
+*
+* @author Antonio Luis Garcia
+*/
+
 package ejercicio9;
 import java.util.ArrayList;
 import java.util.Collections;
-
 
 public class ejercicio9{
     public static void main(String[] args) {
