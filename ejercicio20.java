62c844bab0606ff08e372d975934442d18acd0de
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-01-30 08:22:18 +0100

    ejercicio 20

--- ejercicio20.java
+++ ejercicio20.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-//
+
 public class ejercicio20 {
     public static void main(String[] args) {
 
